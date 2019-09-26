fd6241a1970c519e425b6ceea1a2403056da05ee
zfhrp <coricozizi@gmail.com>
2019-09-26 16:43:45 +0900

    add: answer_1.rs
    - gitignore (for rust, .idea)
    - Cargo init

---
+++ Question_01_10/answers_rust/answer_1.rs
@@ -0,0 +1,34 @@
+use image::DynamicImage::*;
+use std::env;
+use std::path::Path;
+use std::process;
+
+fn main() {
+    let img = image::open(&Path::new("assets/imori.jpg"))
+        .ok()
+        .expect("open error");
+
+    let mut new_img: Vec<u8> = Vec::new();
+    for rgb in img.raw_pixels().chunks(3) {
+        new_img.push(rgb[2]);
+        new_img.push(rgb[1]);
+        new_img.push(rgb[0]);
+    }
+
+    let img = match img {
+        ImageRgb8(img) => img,
+        _ => {
+            eprintln!("image");
+            process::exit(1);
+        }
+    };
+    let (width, height) = (img.width(), img.height());
+
+    let new_image = image::save_buffer(
+        &Path::new("Question_01_10/answers_rust/answer_1.jpg"),
+        &new_img,
+        width,
+        height,
+        image::ColorType::RGB(8),
+    );
+}
